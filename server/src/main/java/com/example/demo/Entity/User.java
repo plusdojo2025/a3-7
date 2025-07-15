d2144264ff457d6bfe2c28147a0aeae90d1a088d
plusdojo2025-senzaki <senzaki-hiroki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-15 16:09:18 +0900

    【Entity】User

--- server/src/main/java/com/example/demo/Entity/User.java
+++ server/src/main/java/com/example/demo/Entity/User.java
@@ -1,5 +1,25 @@
 package com.example.demo.Entity;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer user_id;
+    private String email;
+    private String password;
+    private Integer company_code;
+    private String name;
 }
