04faad120c85be5a67bc1758a9143d1029ac5f82
plusdojo2025-Hirose <hirose-hajime-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-15 15:49:10 +0900

    Entityを変更

--- server/src/main/java/com/example/demo/Entity/Equipment.java
+++ server/src/main/java/com/example/demo/Entity/Equipment.java
@@ -1,8 +1,28 @@
 package com.example.demo.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="equipments")
 public class Equipment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer equip_id;
+	private String  equip_name;
+	private Integer equip_kind_id;
+	private Integer equip_detail_id;
+	private Integer project_id;
+	
 
 }
--- server/src/main/java/com/example/demo/Entity/Process.java
+++ server/src/main/java/com/example/demo/Entity/Process.java
@@ -1,8 +1,25 @@
 package com.example.demo.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="processes")
 public class Process {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer process_id;
+	private String  process_mame;
+	private Integer project_id;
+	private Integer complete;
 }
