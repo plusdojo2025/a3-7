9c761beb344b26bc504ca353061a998afe6fa556
Maung-123 <su-lae-maung-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-15 16:06:42 +0900

    Entity(Reflect/Report)

--- server/src/main/java/com/example/demo/Entity/Reflect.java
+++ server/src/main/java/com/example/demo/Entity/Reflect.java
@@ -1,5 +1,27 @@
 package com.example.demo.Entity;
 
-public class Reflect {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="reflects")
+
+public class Reflect {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer reflectId;
+	private Integer processId;
+	private Integer projectId;
+	private Integer reflectTagId;
+	private String createdAt;
+    private String comment;
 }
