55bd58c2456a76cff04ad7ff0773efbc8accb5bd
plusdojo2025-tanada <tanada-rui-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-15 16:15:26 +0900

    [Entity]ReflectTag,ProjectTag,Unit

--- server/src/main/java/com/example/demo/Entity/Unit.java
+++ server/src/main/java/com/example/demo/Entity/Unit.java
@@ -1,5 +1,29 @@
 package com.example.demo.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="units")
+
 public class Unit {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "unit_id", nullable = false)
+	private Integer unitId;
+	
+	@Column(name = "unit", nullable = false, length = 50)
+	private String unit;
 
 }
