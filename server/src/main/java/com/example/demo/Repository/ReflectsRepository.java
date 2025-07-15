2533294d938852835a41e2edd34ccfb329ce2925
Maung-123 <su-lae-maung-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-15 15:32:20 +0900

    エーラ解決

--- server/src/main/java/com/example/demo/Repository/ReflectsRepository.java
+++ server/src/main/java/com/example/demo/Repository/ReflectsRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReflectsRepository extends JpaRepository<T, ID> {
+import com.example.demo.Entity.Reflects;
+
+public interface ReflectsRepository extends JpaRepository<Reflects, Integer> {
 
 }
