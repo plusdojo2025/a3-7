6cd7651ede55287e268511101a4e51f9bcedcc9e
plusdojo2025-Hirose <hirose-hajime-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-15 16:35:48 +0900

    data.sqlを作成、propertiesを更新

--- server/src/main/java/com/example/demo/Repository/EquipmentsRepository.java
+++ server/src/main/java/com/example/demo/Repository/EquipmentsRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.Entity.Equipment;
-
 public interface EquipmentsRepository extends JpaRepository<Equipment, Integer> {
 
 }
