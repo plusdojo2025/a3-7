00ab83b2b4f80c5db1c6b76f4eba8efd35748d27
plusdojo2025-tanada <tanada-rui-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-16 14:44:01 +0900

    [プロ検関連]Controll,Entity,Repository,js

---
+++ client/src/Search.js
@@ -0,0 +1,85 @@
+import React from "react";
+import axios from "axios";
+
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            title: "",
+            tagId: "",
+            tags: [],
+            projects: []
+        };
+    }
+
+    //初期マウント時にタグ一覧取得
+    componentDidMount() {
+        axios.get("/api/project-tags")
+        .then(res => {
+            this.setState({tags: res.data});
+        })
+        .catch(err => {
+            console.error("タグ一覧の取得に失敗しました。", err);
+        });    
+    }
+
+    //入力が変更されたときstateを更新
+    onInput = (e) => {
+        const name = e.target.name;
+        this.setState({ [name]: e.target.value});
+    }
+
+    //検索ボタン処理
+    searchProjects = () => {
+        const { title, tagId } = this.state;
+
+        const params = {
+            title: title,
+            tagId: tagId
+        };
+
+    axios.get("/api/projects/search", { params })
+      .then(res => {
+        this.setState({ projects: res.data });
+      })
+      .catch(err => {
+        console.error("検索に失敗しました", err);
+      });
+    }
+
+    render() {
+        const { title, tagId, tags, projects } = this.state;
+
+        return (
+            <div className="searchMain">
+                <h2>プロジェクト検索</h2>
+                <div classname="searchForm">
+                    プロジェクト名：
+                    <input type="text" name="title" value={title} onChange={this.onInput} /><br />
+                    タグ：
+                    <select name="tagId" value={tagId} onChange={this.onInput}>
+                     <option value="">タグを選択</option>
+                     {tags.map(tag => (
+                        <option key={tag.projectTagId} value={tag.projectTagId}>
+                            {tag.projectTagName}
+                        </option>
+                     ))} 
+                    </select><br />
+                    <button onClick={this.searchProjects}>検索</button>
+                </div>
+
+                {/*検索結果*/}
+                <ul>
+                   {projects.length === 0 && <li>検索結果がありません</li>}
+                   {projects.map(project => (
+                    <li key={project.projectId}>
+                        <a href={`/project/${project.projectId}`}></a>
+                        {" "} - {project.projectTag.projectTagName}
+                    </li> 
+                   ))}
+                </ul>
+            </div>
+        );
+    }
+}
