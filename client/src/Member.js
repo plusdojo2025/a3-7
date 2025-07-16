93d6fdb6b2911454a03274789040439e0eee016d
plusdojo2025-yamamoto <yamamoto-takashi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-16 15:50:40 +0900

    メンバー編集途中

--- client/src/Member.js
+++ client/src/Member.js
@@ -6,21 +6,25 @@ export default class Member extends React.Component {
     render() {
         return (
             <>
-            <header id="nav">
-                <div>
-                    <Link to="/home">
-                        <img src="/img/Labchain.png" className="logo-gazou" alt="Labchain"/>
-                    </Link>
-                </div>
+                <header id="nav">
+                    <div>
+                        <Link to="/home">
+                            <img src="/img/Labchain.png" className="logo-gazou" alt="Labchain" />
+                        </Link>
+                    </div>
 
-                <ul className="nav-links" id="nav-links">
-                    <li><Link to="/home">ホーム</Link></li>
-                    <li><Link to="/mypage">マイページ</Link></li>
-                    <li><Link to="/search">プロジェクト検索</Link></li>
-                    <li><Link to="/login" className="logout-button">ログアウト</Link></li>
-                </ul>
-            </header>
-            
+                    <ul className="nav-links" id="nav-links">
+                        <li><Link to="/home">ホーム</Link></li>
+                        <li><Link to="/mypage">マイページ</Link></li>
+                        <li><Link to="/search">プロジェクト検索</Link></li>
+                        <li><Link to="/login" className="logout-button">ログアウト</Link></li>
+                    </ul>
+                </header>
+                <h1>プロジェクトメンバー編集</h1>
+                <div class="input-wrapper ">
+                    <input type="text" id="mail" name="mail" placeholder="招待したい方のメールアドレスを入力してください" />
+                    <input class="sub_botun" type="submit" name="submit" value="検索"></input>
+                </div>
             </>
         )
     };
