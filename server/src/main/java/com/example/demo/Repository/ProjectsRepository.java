ffb989891390584d8508df2e6a73a85cc08cac5d
plusdojo2025-tanada <tanada-rui-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-16 13:44:57 +0900

    Revert "[プロ検関連]Controll,Entity,Repository,js"
    This reverts commit 9c8b222e27ebad2fa709b41342a2e1016d250b00.

--- server/src/main/java/com/example/demo/Repository/ProjectsRepository.java
+++ server/src/main/java/com/example/demo/Repository/ProjectsRepository.java
@@ -1,16 +1,9 @@
 package com.example.demo.Repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.Entity.Project;
-import com.example.demo.Entity.ProjectTag;
 
 public interface ProjectsRepository extends JpaRepository<Project, Integer>{
-	
-	List<Project> findByProjectNameContainingIgnoreCase(String name);
-	List<Project> findByProjectNameContainingIgnoreCaseAndTag(String name, ProjectTag tag);
-	List<Project> findByTag(ProjectTag tag);
 
 }
