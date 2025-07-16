37a5237205a75b7e6cbec549ea0b4e83f2bf7036
plusdojo2025-yamamoto <yamamoto-takashi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-16 12:00:15 +0900

    背景の色を変えた。メンバー、プロジェクト管理画面変えた

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
+import './css/Common.css';
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 import Mypage from './Mypage';
 import Search from './Search';
-<<<<<<< Updated upstream
 import EquipmentRegist from './EquipmentRegist';
 import BioRegist from './BioRegist';
 import EquipmentEdit from './EquipmentEdit';
 import BioEdit from './BioEdit';
+import Project from './Project';
+import Process from './Process';
+import Member from './Member';
 
-=======
-import Report from './Report';
->>>>>>> Stashed changes
+
+
 
 function App() {
   return (
@@ -20,16 +22,15 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/process" element={<Process />} />
+        <Route path="/member" element={<Member />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/search" element={<Search />} />
-<<<<<<< Updated upstream
        <Route path="/equipmentRegist" element={<EquipmentRegist />} />
         <Route path="/bioRegist" element={<BioRegist />} />
         <Route path="/equipmentEdit" element={<EquipmentEdit />} />
         <Route path="/bioEdit" element={<BioEdit />} />
-=======
-        <Route path="/report" element={<Report />} />
->>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
