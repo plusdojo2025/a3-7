3a9fd55dc4f511eaa302f516649683f42c640fc1
plusdojo2025-senzaki <senzaki-hiroki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-17 13:30:50 +0900

    【編集】編集画面に遷移できるように

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ function App() {
               <Route path="process" element={<Process />} />
               <Route path="member" element={<Member />} />
               <Route path="mypage" element={<Mypage />} />
-              <Route path="mypageedit" element={<MyPageEdit />} />
+              <Route path="mypage/edit" element={<MyPageEdit />} />
               <Route path="mail" element={<Mail />} />
               <Route path="search" element={<Search />} />
               <Route path="project/:projectId" element={<ViewProject />} />
