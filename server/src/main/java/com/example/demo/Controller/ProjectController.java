50af2d81951461e9df3c8e45db0b594f82c1865c
plusdojo2025-Hirose <hirose-hajime-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-17 10:37:40 +0900

    Homeにimportを追加

--- server/src/main/java/com/example/demo/Controller/ProjectController.java
+++ server/src/main/java/com/example/demo/Controller/ProjectController.java
@@ -1,5 +1,19 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Repository.ProjectsRepository;
+
+@RestController
 public class ProjectController {
+	
+	@Autowired
+    private ProjectsRepository projextRepository;
+	
+    @GetMapping("/project/")
+    public List<Project> getMyPloject(HttpSession session) {
+        return 
+    }
 
 }
