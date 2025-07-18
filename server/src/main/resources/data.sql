197d7e02c65cf608d1431b23a2080dad4f9b9b72
plusdojo2025-tanada <tanada-rui-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-18 17:00:32 +0900

    デモデータ追加

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -18,7 +18,7 @@ INSERT INTO equip_kinds (equip_kind_name) VALUES ('道具');
 INSERT INTO equipments (equip_name, equip_kind_id, equip_detail_id, project_id) VALUE ('虫あみ', 1, 1, 1);
 
 --備品詳細
-INSERT INTO equip_details (remaining, limited, judge, storage, remarks, unit, picture) VALUE (100.0, '2025-4-28', 10.0, '倉庫', '所々穴が開いている。', 1, null);
+INSERT INTO equip_details (remaining, limited, judge, storage, remarks, unit, picture) VALUE (100.0, '2025-04-28', 10.0, '倉庫', '所々穴が開いている。', 1, null);
 
 --単位
 INSERT INTO units (unit) VALUES ('本'),('個'),('箱'),('kg'),('g'),('mg'),('L'),('ml');
@@ -32,9 +32,13 @@ INSERT INTO project_reports (project_id, created_at, report) VALUES (1, '2024-03
 INSERT INTO project_reports (project_id, created_at, report) VALUES (1, '2024-05-15', '中間報告：これまでに約50種のハナアブを捕獲。未識別の種も複数確認。');
 
 --日報
+INSERT INTO reports (process_id, project_id, created_at, comment) VALUE (1, 1, '2024-03-25', '明日からがんばる！');
 
 --反省
+INSERT INTO reflects (process_id, project_id, created_at, reflect_tag_id, comment) VALUE (1, 1, '2024-03-25', 1, '寝坊した。');
 
+--反省タグ
+INSERT INTO reflect_tags (reflect_name) VALUE ('災害');
 
 /*
 INSERT INTO equipments (
